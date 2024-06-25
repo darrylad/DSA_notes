5018d2aef875504b7e24e2d3d4db324e8575fb9c
Darryl David <darrylcad@gmail.com>
2024-06-25 12:52:18 +0530

    25th June

---
+++ Step2_Learn_important_sorting_techniques/Lec1_Sorting_I/P1_Selection_sort/N4_code.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped)
+            break;
+    }
+}
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int j = i;
+        while (j >= 0 && arr[j - 1] > arr[j])
+        {
+            swap(arr[j - 1], arr[j]);
+            j--;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    insertionSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << ' ';
+    return 0;
+}
